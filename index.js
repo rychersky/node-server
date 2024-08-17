68fa9eb2cfd412f7149f482aa0910ec58abc4dea
rychersky <rychersky@gmail.com>
2024-08-17 11:45:06 -0500

    learningggg

--- index.js
+++ index.js
@@ -5,27 +5,19 @@ const app = express();
 const options = { root: import.meta.dirname };
 
 app.get(['/', '/index'], (req, res) => {
-  res.sendFile('index.html', options, (e) => {
-    if (e) console.log(e);
-  });
+  res.sendFile('index.html', options);
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile('about.html', options, (e) => {
-    if (e) console.log(e);
-  });
+  res.sendFile('about.html', options);
 });
 
 app.get('/contact-me', (req, res) => {
-  res.sendFile('contact-me.html', options, (e) => {
-    if (e) console.log(e);
-  });
+  res.sendFile('contact-me.html', options);
 });
 
 app.get('*', (req, res) => {
-  res.sendFile('404.html', options, (e) => {
-    if (e) console.log(e);
-  });
+  res.sendFile('404.html', options);
 });
 
 app.listen(8080);
